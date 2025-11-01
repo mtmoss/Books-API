d546a160fc570cbf4fa49a6ae1bb324043570425
Maria Thereza Moss <mt@mtmoss.co>
2025-11-01 15:11:32 -0300

    redireciona a home para o swagger

--- index.py
+++ index.py
@@ -6,13 +6,15 @@ from api.routes.auth import route_auth
 
 app = FastAPI(
     title="Book Scraping API",
+    docs_url="/docs",
+    openapi_url="/openapi.json"
 )
 
 app.get("/", include_in_schema=False)
 def health():
     return {"status": "ok"}
 def root():
-    return RedirectResponse("/docs", status_code=307)
+    return RedirectResponse(url=app.docs_url, status_code=307)
 
 app.include_router(route_health)
 app.include_router(route_books)
