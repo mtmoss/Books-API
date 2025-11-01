ba07e442a153c6092c3b4431f8cd5ac998add526
Maria Thereza Moss <mt@mtmoss.co>
2025-11-01 14:23:53 -0300

    conserta erro na rota de categorias

--- api/routes/books.py
+++ api/routes/books.py
@@ -4,14 +4,14 @@ from ..models import Book, OutputBooks
 from ..repository import get_book_by_id, list_books, list_categories
 from ..services import filter_books
 
-route_books = APIRouter(prefix="/api/v1", tags=["books"])
+route_books = APIRouter(prefix="/api/v1/books", tags=["books"])
 
-@route_books.get("/books", response_model=OutputBooks)
+@route_books.get("/", response_model=OutputBooks)
 def list_all_books():
     items = list_books()
     return {"total": len(items), "items": items}
 
-@route_books.get("/books/search", response_model=OutputBooks)
+@route_books.get("/search", response_model=OutputBooks)
 def search_books(
     title: Optional[str] = Query(default=None),
     category: Optional[str] = Query(default=None)
@@ -20,13 +20,13 @@ def search_books(
     items_filter = filter_books(items, title=title, category=category)
     return {"total": len(items_filter), "items": items_filter}
 
-@route_books.get("/books/{id}", response_model=Book)
+@route_books.get("/categories", response_model=List[str])
+def get_categories():
+    return list_categories()
+
+@route_books.get("/{id}", response_model=Book)
 def detail_book(id: int):
     book = get_book_by_id(id)
     if not book:
         raise HTTPException(status_code=404, detail="Livro não encontrado")
     return book
-
-@route_books.get("/books/categories", response_model=List[str])
-def get_categories():
-    return list_categories()
